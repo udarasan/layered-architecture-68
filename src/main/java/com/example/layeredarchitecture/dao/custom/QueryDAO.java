4a14e9d0bfb213863c143424c7fd9a486e17c8be
udarasan <udarassanjeewa@gmail.com>
2023-12-23 11:57:08 +0530

    introduce DAO Factory and SuperDAO

--- src/main/java/com/example/layeredarchitecture/dao/custom/QueryDAO.java
+++ src/main/java/com/example/layeredarchitecture/dao/custom/QueryDAO.java
@@ -1,7 +1,8 @@
 package com.example.layeredarchitecture.dao.custom;
 
+import com.example.layeredarchitecture.dao.SuperDAO;
 import com.example.layeredarchitecture.model.CustomDTO;
 
-public interface QueryDAO {
+public interface QueryDAO extends SuperDAO {
     void customerOrderDetails(CustomDTO customDTO);
 }
